e6cb64b607b447c7eb565e86b825175731dee4f1
Alexey <lbonanzza@gmail.com>
2020-01-31 17:01:15 +0300

    ad article list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ArticleList from "./components/article-list";
+import articles from './fixtures'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Articles App</h1>
+        <ArticleList articles={articles}/>
+      </div>
+    );
+  }
 }
 
 export default App;
